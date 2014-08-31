966cec60b422e482d99a28601f94368ce402a3df
Pavel Agalecky <pavel.agalecky@gmail.com>
2014-08-31 22:52:41 +0400

    Added messages translation config

---
+++ Bootstrap.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace omnilight\phonenumbers;
+use yii\base\Application;
+use yii\base\BootstrapInterface;
+
+
+/**
+ * Class Bootstrap
+ * @package \omnilight\phonenumbers
+ */
+class Bootstrap implements BootstrapInterface
+{
+
+    /**
+     * Bootstrap method to be called during application bootstrap stage.
+     * @param Application $app the application currently running
+     */
+    public function bootstrap($app)
+    {
+        $app->i18n->translations['omnilight/phonenumbers'] = [
+            'class' => 'yii\i18n\PhpMessageSource',
+            'basePath' => '@omnilight/phonenumbers/messages',
+            'sourceLanguage' => 'en-US',
+        ];
+    }
+}
